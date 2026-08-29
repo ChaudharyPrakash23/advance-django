cdbed36d8df5c16cfa24bd08298d61f431900d5c
prakash chaudhary <chaudharyprakash023pc@gmail.com>
2026-08-29 20:33:18 +0545

    clearing in memory cache

--- project34/youtube/admin.py
+++ project34/youtube/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.core.cache import cache
+
 from .models import YouTubeUser
 
 
-# Register your models here.
+@admin.action(description='Clear user Cache')
+def clear_users_cache(modeladmin, request, queryset):
+    cache.delete('users_data')
+    messages.success(request, 'Users Cache cleared successfully')
+
+
 @admin.register(YouTubeUser)
 class YouTubeUserAdmin(admin.ModelAdmin):
-    list_display=('name','email','Subscribers')
-    
+    list_display = ('name', 'email', 'Subscribers')
+    actions = [clear_users_cache]
--- project34/youtube/tests.py
+++ project34/youtube/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from youtube.admin import YouTubeUserAdmin, clear_users_cache
 
-# Create your tests here.
+
+class YouTubeUserAdminTests(TestCase):
+    def test_custom_cache_action_is_registered(self):
+        self.assertIn(clear_users_cache, YouTubeUserAdmin.actions)
